72c7e91fdb2e6dd88482cb70e9b32bfcf0763e84
Geoffrey Zimmerman <geoffzimmerman23@gmail.com>
2020-12-15 16:42:59 -0600

    query urls established, added text to present day fields

--- script.js
+++ script.js
@@ -4,12 +4,41 @@ $( document ).ready(function() {
 
         var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + query_param + "&units=imperial&cnt=5&APPID=15dbd29d44cd5bae9c5c65cfeba9be16"
 
-    
+        var queryFiveDay = "http://api.openweathermap.org/data/2.5/forecast?q=" + query_param + "&appid=15dbd29d44cd5bae9c5c65cfeba9be16"
+
         $.ajax({
         url: queryURL,
         method: "GET"
         }).then(function(weather) {
-          console.log(weather);
+            console.log(weather)
+            var locusLon = weather.coord.lon
+
+            var locusLat = weather.coord.lat
+
+            var uviURL = "http://api.openweathermap.org/data/2.5/uvi?lat=" + locusLat + "&lon=" + locusLon + "&appid=15dbd29d44cd5bae9c5c65cfeba9be16"
+
+            $("#search-city").text(weather.name)
+            $("#temp").text("Temperature: " + Math.round(weather.main.temp) + "F")
+            $("#humidity").text("Humidity: " + weather.main.humidity + "%")
+            
+            $.ajax({
+                url: uviURL,
+                method: "GET"
+            }).then(function(uvi) {
+                var uvIndex = uvi.value
+                var uvDisplay =  $("#uv-index")
+                uvDisplay.text("UV Index: " + uvIndex)
+                if (uvIndex < 3) {
+                    uvDisplay.attr("class", "uv-good")
+                }
+            })
+        })
+        $.ajax({
+        url: queryFiveDay,
+        method: "GET"
+        }).then(function(fiveDay) {
+            console.log(fiveDay);
+
         })
     })
 
