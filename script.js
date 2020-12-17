ce4f28f54aa5adc1b133e91713c61dc41d47802e
Geoffrey Zimmerman <geoffzimmerman23@gmail.com>
2020-12-16 19:02:53 -0600

    set five day forcast display

--- script.js
+++ script.js
@@ -2,15 +2,17 @@ $( document ).ready(function() {
     $("#search").on('click', function(){
         var query_param = $(this).prev().val();
 
-        var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + query_param + "&units=imperial&cnt=5&APPID=15dbd29d44cd5bae9c5c65cfeba9be16"
+        var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + query_param + "&units=imperial&APPID=15dbd29d44cd5bae9c5c65cfeba9be16"
 
-        var queryFiveDay = "http://api.openweathermap.org/data/2.5/forecast?q=" + query_param + "&appid=15dbd29d44cd5bae9c5c65cfeba9be16"
+        var queryFiveDay = "http://api.openweathermap.org/data/2.5/forecast?q=" + query_param + "&units=imperial&appid=15dbd29d44cd5bae9c5c65cfeba9be16"
+
+        localStorage.setItem("Query", $(this).prev().val())
 
         $.ajax({
         url: queryURL,
         method: "GET"
         }).then(function(weather) {
-            console.log(weather)
+            // console.log(weather)
             var locusLon = weather.coord.lon
 
             var locusLat = weather.coord.lat
@@ -20,7 +22,7 @@ $( document ).ready(function() {
             $("#search-city").text(weather.name + " (" + moment().format("MMMM Do") + ")")
             $("#temp").text("Temperature: " + Math.round(weather.main.temp) + "F")
             $("#humidity").text("Humidity: " + weather.main.humidity + "%")
-            $("#weather-icon").attr("src", "http://openweathermap.org/img/w/" + weather.weather[0].icon + ".png")
+            $("#weather-icon").attr({"src": "http://openweathermap.org/img/w/" + weather.weather[0].icon + ".png", "alt": "Weather Icon"})
             
             $.ajax({
                 url: uviURL,
@@ -50,8 +52,47 @@ $( document ).ready(function() {
         url: queryFiveDay,
         method: "GET"
         }).then(function(fiveDay) {
-            console.log(fiveDay);
+            var arrayOfDays = [fiveDay.list[4],
+            fiveDay.list[12],
+            fiveDay.list[20],
+            fiveDay.list[28],
+            fiveDay.list[36],]
 
+            var daysDisplay = [$("#day1"),
+            $("#day2"),
+            $("#day3"),
+            $("#day4"),
+            $("#day5"),]
+
+            var iconDisplay = [$("#weather-icon1"),
+            $("#weather-icon2"),
+            $("#weather-icon3"),
+            $("#weather-icon4"),
+            $("#weather-icon5"),]
+
+            var tempDisplay = [$("#temp1"),
+            $("#temp2"),
+            $("#temp3"),
+            $("#temp4"),
+            $("#temp5"),]
+
+            var humDisplay = [$("#humidity1"),
+            $("#humidity2"),
+            $("#humidity3"),
+            $("#humidity4"),
+            $("#humidity5"),]
+            
+            console.log(arrayOfDays)
+            for (var i =0; i < 5; i++) {
+                
+                daysDisplay[i].text(moment(arrayOfDays[i].dt_txt).format("M/D"))
+                
+                iconDisplay[i].attr({"src": "http://openweathermap.org/img/w/" + arrayOfDays[i].weather[0].icon + ".png", "alt": "Weather Icon"})
+
+                tempDisplay[i].text("Temperature: " + Math.round(arrayOfDays[i].main.temp) + "F")
+
+                humDisplay[i].text("Humidity: " + arrayOfDays[i].main.humidity + "%")
+            }
         })
     })
 
